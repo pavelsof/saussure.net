9a517903822156ab2518a80ca9ae2578c6b07594
pavelsof <pavelsof@gmail.com>
2014-04-26 23:13:52 +0300

    Started user testing.

--- users/tests.py
+++ users/tests.py
@@ -1,3 +1,29 @@
 from django.test import TestCase
+from django.test.client import Client
 
-# Create your tests here.
+import uuid
+
+
+class RegistrationTestCase(TestCase):
+	def test_different_passwords(self):
+		c = Client()
+		string_one = uuid.uuid4()
+		while True:
+			string_two = uuid.uuid4()
+			if string_one != string_two:
+				break
+		response = c.post('/users/registration/', {
+			'email': 'mail@yatdesign.com',
+			'password': string_one,
+			'password2': string_two
+		})
+		self.assertEqual(response.status_code, 200)
+
+
+class LoginTestCase(TestCase):
+	pass
+
+
+class ProfileTestCase(TestCase):
+	pass
+
